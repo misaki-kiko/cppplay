6bc18041f77cc84677962e1b487cb44eac0c4277
misaki-kiko <misaki@futa.in>
2018-01-21 11:18:56 +0800

    练习1.13-3

--- library.cpp
+++ library.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
-    for (int i = 10; i >= 0; i--) {
-        std :: cout << i << std :: endl;
+    int num1,num2;
+    std :: cout << "enter two numbers :" <<std :: endl;
+    std :: cin >> num1 >> num2 ;
+    for (int i = num1; i <= num2 ; ++i) {
+        std :: cout << i << std ::endl;
     }
     return 0;
 }
