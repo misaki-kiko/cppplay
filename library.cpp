50bff297a43b9f78b12c37e67799aaff7ec553c4
misaki-kiko <misaki@futa.in>
2018-01-21 13:27:01 +0800

    练习1.16

--- library.cpp
+++ library.cpp
@@ -2,11 +2,9 @@
 
 int main()
 {
-    int num1,num2;
-    std :: cout << "enter two numbers :" <<std :: endl;
-    std :: cin >> num1 >> num2 ;
-    for (int i = num1; i <= num2 ; ++i) {
-        std :: cout << i << std ::endl;
-    }
+    int sum = 0, value = 0;
+    while (std::cin >> value)
+        sum += value;
+    std ::cout << "sum is :" <<sum << std ::endl;
     return 0;
 }
