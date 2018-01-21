3450c934c404583288ce5a44ce923d40ff74fdd2
misaki-kiko <misaki@futa.in>
2018-01-21 11:07:29 +0800

    练习1.12

--- library.cpp
+++ library.cpp
@@ -2,25 +2,12 @@
 
 int main()
 {
-    int num1,num2;
-    int mid=0;
+    int sum=0;
 
-    std :: cout << "enter two numbers :" << std :: endl;
-    std :: cin >> num1 >> num2 ;
+    for (int i = -100 ; i <= 100 ; ++i)
+        sum += i ;
 
-    if (num1>num2)
-    {
-        mid=num2;
-        num2=num1;
-        num1=mid;
-    }
-
-    while (num1<=num2)
-    {
-        std :: cout << num1 <<std :: endl ;
-        num1++;
-    }
-
+    std :: cout << sum << std :: endl ;
 
     return 0;
 }
