d47cecf60bdcd96396d8da6f73102a7f32d80e1b
misaki-kiko <misaki@futa.in>
2018-01-21 11:16:31 +0800

    练习1.13-2

--- library.cpp
+++ library.cpp
@@ -2,11 +2,8 @@
 
 int main()
 {
-    int sum = 0 ;
-
-    for (int i = 50; i < 100 ; ++i) {
-        sum += i ;
+    for (int i = 10; i >= 0; i--) {
+        std :: cout << i << std :: endl;
     }
-    std :: cout << sum << std :: endl;
     return 0;
 }
